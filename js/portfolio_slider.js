1af997aac0c3bc1cfcbfbd5aede26e9a85f3ac02
Carla Soares <Carla Soares>
2017-03-26 17:40:13 -0300

    first

---
+++ js/portfolio_slider.js
@@ -0,0 +1,149 @@
+$(function(){
+$("#elastic_grid_demo").elastic_grid({
+	'hoverDirection': true,
+	'hoverDelay': 0,
+	'hoverInverse': false,
+	'expandingSpeed': 500,
+	'expandingHeight': 500,
+	'items' :
+		[
+
+
+			{
+			'title' : 'Gelita',
+			'description'   : 'Software para gerenciamento e impressão de etiquetas referentes aos produtos.',
+			'thumbnail' : ['img/portfolio/small/23.jpg'],
+			'large' : ['img/portfolio/large/23.jpg'],
+			'button_list'   :
+			[
+
+			],
+			'tags'  : ['Sistemas Web','Sustentação Software','Todos']
+			},
+
+			{
+			'title' : 'Clube Paineiras',
+			'description'   : 'Software que registrava visitantes.',
+			'thumbnail' : ['img/portfolio/small/24.png'],
+			'large' : ['img/portfolio/large/24.png'],
+			'button_list'   :
+			[
+
+			],
+			'tags'  : ['Sistemas Desktop','Controle Acesso','Todos']
+			},
+
+			{
+			'title' : 'Prossegur',
+			'description'   : 'Aplicativo de integração com o Lenel e Atlas Schindler para elevadores que indicam o elevador que o usuário deve usar.',
+			'thumbnail' : ['img/portfolio/small/2.png'],
+			'large' : ['img/portfolio/large/2.png'],
+			'button_list'   :
+			[
+			],
+			'tags'  : ['Sistemas Desktop','Controle Acesso','Socket','Todos']
+			},
+
+			{
+			'title' : 'Everis - Mitsui Seguros',
+			'description'   : 'Projeto definitivas relacionado a sustentação de sistemas legados. Projeto Contabilidade Analítica e FIPECAFI referente unificação das bases de dados contábeis do sistema core e sistemas legados, visando extrair as informações oficiais de uma única base de dados e extração dos Registros Oficiais, de acordo com o estabelecido pela SUSEP na Circular 360. ',
+			'thumbnail' : ['img/portfolio/small/11.png'],
+			'large' : ['img/portfolio/large/11.png'],
+			'button_list'   :
+			[
+			],
+			'tags'  : ['Sistemas Web','Sustentação Software','BI','Todos']
+			},
+
+
+			{
+			'title' : 'Everis - Ultracargo',
+			'description'   : 'Sustentação de Software.',
+			'thumbnail' : ['img/portfolio/small/13.jpg'],
+			'large' : ['img/portfolio/large/13.jpg'],
+			'button_list'   :
+			[
+			],
+			'tags'  : ['Sistemas Web','Sustentação Software','Todos']
+			},
+
+	 		{
+			'title' : 'Everis - Telefônica',
+			'description'   : 'Projeto Base Única referente aos clientes e reparos',
+			'thumbnail' : ['img/portfolio/small/14.jpg'],
+			'large' : ['img/portfolio/large/14.jpg'],
+			'button_list'   :
+			[
+			],
+			'tags'  : ['Sistemas Desktop','Todos']
+			},
+
+			{
+			'title' : 'Eicon',
+			'description'   : 'Solução para sistema público de saúde.',
+			'thumbnail' : ['img/portfolio/small/15.png'],
+			'large' : ['img/portfolio/large/15.png'],
+			'button_list'   :
+			[
+			],
+			'tags'  : ['Sistemas Web','Todos']
+			},
+
+			{
+			'title' : 'Direção',
+			'description'   : 'Controle de pin pads e testes.',
+			'thumbnail' : ['img/portfolio/small/16.png'],
+			'large' : ['img/portfolio/large/16.png'],
+			'button_list'   :
+			[
+			],
+			'tags'  : ['Excel','Todos']
+			},
+
+			{
+			'title' : 'CheckExpress',
+			'description'   : 'Projeto de correspondente bancário e gateway bancário para o banco Bradesco.',
+			'thumbnail' : ['img/portfolio/small/17.png'],
+			'large' : ['img/portfolio/large/17.png'],
+			'button_list'   :
+			[
+			],
+			'tags'  : ['Sistemas Desktop','Socket','Sistemas Web','Todos']
+			},
+
+			{
+			'title' : 'BRQ - Volkswagen',
+			'description'   : 'Sustentação do sistema de financiamento do banco.',
+			'thumbnail' : ['img/portfolio/small/19.png'],
+			'large' : ['img/portfolio/large/19.png'],
+			'button_list'   :
+			[
+			],
+			'tags'  : ['Sustentação Software','Todos']
+			},
+
+			{
+			'title' : 'Bernhoeft',
+			'description'   : 'Sistema Portal canal de interação entre empresa audidata e empresa de auditoria. Sistema Expert cálculos trabalhistas. Sistema Contract referente a risco de terceiros',
+			'thumbnail' : ['img/portfolio/small/20.jpg'],
+			'large' : ['img/portfolio/large/20.jpg'],
+			'button_list'   :
+			[
+			],
+			'tags'  : ['Sistemas Web','Sustentação Software','Todos']
+			},
+			{
+			'title' : 'Lolla Paola Decorações',
+			'description'   : 'Lolla Paola Decorações é um site projetado para demonstrar os modelos de decoração já realizados e seus produtos. Foi arquitetado para ter inclusão de fotos de forma fácil através do Json e para ler esse arquivo foi usado angular.js.',
+			'thumbnail' : ['img/portfolio/small/25.png'],
+			'large' : ['img/portfolio/large/25.png'],
+			'button_list'   :
+			[
+			{ 'title':'Dê uma olhada', 'url' : 'http://lolladecoraprovencal.atspace.cc'},
+			//{ 'title':'Download', 'url':'http://#'}
+			],
+			'tags'  : ['Site','Todos']
+			},
+		]
+	});
+});
